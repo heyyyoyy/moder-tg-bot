cf1fa72e2fffff147c0806017b2a876ca4142e56
heyyyoyy <heyyyoyy@gmail.com>
2019-01-27 16:29:56 +0300

    add and remove testing

--- handlers.py
+++ handlers.py
@@ -1,15 +1,8 @@
-from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.types import ContentType
+from aiogram.utils.exceptions import MessageCantBeDeleted
 import logging
 
-from settings import TOKEN
-
-
-logging.basicConfig(level=logging.INFO)
-
-bot = Bot(TOKEN, parse_mode='HTML')
-storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage)
+from settings import bot, dp, BOT_ID
 
 
 @dp.errors_handler()
@@ -23,4 +16,35 @@ async def welcome(message):
     await bot.send_message(
         message.from_user.id,
         'hello'
+    )
+
+
+@dp.message_handler(content_types=ContentType.NEW_CHAT_MEMBERS)
+async def new_chat_members_handler(message):
+    for member in message.new_chat_members:
+        if member.id == BOT_ID:
+            # Bot added in the group
+            pass
+        else:
+            # User added in the group, check him and try delete service message
+            pass
+
+
+@dp.message_handler(content_types=ContentType.LEFT_CHAT_MEMBER)
+async def left_user_handler(message):
+    if message.left_chat_member.id == BOT_ID:
+        # Bot was removed from the group
+        pass
+    else:
+        # User left from the group. Try delete service message
+        pass
+
+
+@dp.message_handler(lambda msg: msg.chat.type in ['group', 'supergroup'])
+async def all(message):
+    from aiogram.types import ForceReply
+    await bot.send_message(
+        message.from_user.id,
+        'hih',
+        reply_markup=ForceReply()
     )
--- settings.py
+++ settings.py
@@ -1,6 +1,27 @@
 import os
 from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher
+from aiogram.types import ParseMode
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import logging
+import asyncio
 
 load_dotenv()
 
+loop = asyncio.get_event_loop()
+
+logging.basicConfig(level=logging.INFO)
+
 TOKEN = os.environ['TOKEN']
+
+
+bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+
+async def get_bot_id():
+    bot_ = await bot.get_me()
+    return bot_.id
+
+BOT_ID = loop.run_until_complete(get_bot_id())
