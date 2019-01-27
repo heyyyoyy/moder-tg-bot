6e20860bc840dc3864dd6ac95dbaa5212c0344c2
heyyyoyy <heyyyoyy@gmail.com>
2019-01-27 17:31:44 +0300

    supporting comments created

--- handlers.py
+++ handlers.py
@@ -24,9 +24,16 @@ async def new_chat_members_handler(message):
     for member in message.new_chat_members:
         if member.id == BOT_ID:
             # Bot added in the group
+            # Save group in the db 
+            # or check mark that the group is deleted (deleted=False)
             pass
         else:
-            # User added in the group, check him and try delete service message
+            # User added in the group
+            # Use restrictChatMember
+            # Send message with button
+            # and try delete service message
+            # Save user in the db (group - user) 
+            # or check mark that user is deleted (deleted=False)
             pass
 
 
@@ -34,17 +41,16 @@ async def new_chat_members_handler(message):
 async def left_user_handler(message):
     if message.left_chat_member.id == BOT_ID:
         # Bot was removed from the group
+        # Mark in the db that the group is deleted
         pass
     else:
-        # User left from the group. Try delete service message
+        # User left from the group
+        # Try delete service message
+        # Mark in the db that user is deleted
         pass
 
 
 @dp.message_handler(lambda msg: msg.chat.type in ['group', 'supergroup'])
 async def all(message):
-    from aiogram.types import ForceReply
-    await bot.send_message(
-        message.from_user.id,
-        'hih',
-        reply_markup=ForceReply()
-    )
+    # Check messages in the group (link, media, stickers)
+    pass
