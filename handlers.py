2f5cdd903ccb9d6d65723e60d58af5c096c8e382
heyyyoyy <heyyyoyy@gmail.com>
2019-01-30 22:52:12 +0300

    instance user/group/usertogroup created/deleted

--- handlers.py
+++ handlers.py
@@ -4,6 +4,8 @@ import logging
 
 from settings import bot, dp, BOT_ID
 
+from models import manager, UserToGroup, Group
+
 
 @dp.errors_handler()
 async def error(update, error):
@@ -24,11 +26,16 @@ async def new_chat_members_handler(message):
     for member in message.new_chat_members:
         if member.id == BOT_ID:
             # Bot added in the group
+            await Group.save_group(message.chat, readded=True)
             # Save group in the db
             # or check mark that the group is deleted (deleted=False)
             pass
         else:
             # User added in the group
+            await UserToGroup.save_user_to_group(
+                member,
+                message.chat
+            )
             # Use restrictChatMember
             # Send message with button
             # and try delete service message
@@ -41,10 +48,15 @@ async def new_chat_members_handler(message):
 async def left_user_handler(message):
     if message.left_chat_member.id == BOT_ID:
         # Bot was removed from the group
+        await Group.delete_group(message.chat.id)
         # Mark in the db that the group is deleted
         pass
     else:
         # User left from the group
+        await UserToGroup.delete_user_from_group(
+            message.left_chat_member,
+            message.chat.id
+        )
         # Try delete service message
         # Mark in the db that user is deleted
         pass
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import peewee
 import peewee_async
 from datetime import datetime
+import logging
 
 from settings import DB_NAME, DB_USER, DB_PASSWORD
 
@@ -13,6 +14,7 @@ db = peewee_async.PooledPostgresqlDatabase(
     autorollback=True)
 
 manager = peewee_async.Manager(db)
+manager.allow_sync = logging.WARNING
 
 
 class Base(peewee.Model):
@@ -27,6 +29,19 @@ class User(Base):
     registration = peewee.DateTimeField(default=datetime.now)
     admin = peewee.BooleanField(default=False)
 
+    @classmethod
+    async def save_user(cls, user):
+        try:
+            _user = await manager.get(cls, id=user.id)
+        except cls.DoesNotExist:
+            _user = await manager.create(
+                    cls,
+                    id=user.id,
+                    name=user.first_name,
+                    username=user.username
+                )
+        return _user
+
 
 class Group(Base):
     id = peewee.BigIntegerField(primary_key=True)
@@ -35,6 +50,33 @@ class Group(Base):
     registration = peewee.DateTimeField(default=datetime.now)
     deleted = peewee.BooleanField(default=False)
 
+    @classmethod
+    async def save_group(cls, group, readded=False):
+        try:
+            _group = await manager.get(cls, id=group.id)
+        except cls.DoesNotExist:
+            _group = await manager.create(
+                    cls,
+                    id=group.id,
+                    title=group.title,
+                    username=group.username
+                )
+        else:
+            if readded:
+                _group.deleted = False
+                await manager.update(_group)
+        return _group
+
+    @classmethod
+    async def delete_group(cls, group_id):
+        async with manager.transaction():
+            group = await manager.get(
+                cls,
+                id=group_id
+                )
+            group.deleted = True
+            await manager.update(group)
+
 
 class UserToGroup(Base):
     user = peewee.ForeignKeyField(User, related_name='to_user', on_delete='CASCADE')
@@ -42,6 +84,34 @@ class UserToGroup(Base):
     registration = peewee.DateTimeField(default=datetime.now)
     deleted = peewee.BooleanField(default=False)
 
+    @classmethod
+    async def save_user_to_group(cls, user, group):
+        async with manager.transaction():
+            await User.save_user(user)
+            await Group.save_group(group)
+            try:
+                _group = await manager.get(cls, user=user.id, group=group.id)
+            except cls.DoesNotExist:
+                await manager.create(
+                    cls,
+                    user=user.id,
+                    group=group.id)
+            else:
+                _group.deleted = False
+                await manager.update(_group)
+
+    @classmethod
+    async def delete_user_from_group(cls, user_id, group_id):
+        async with manager.transaction():
+            user_group = await manager.get(
+                cls,
+                user=user_id,
+                group=group_id
+                )
+            user_group.deleted = True
+            await manager.update(user_group)
+
 
 if __name__ == "__main__":
-    db.create_tables([User, Group, UserToGroup])
+    with manager.allow_sync():
+        db.create_tables([User, Group, UserToGroup])
